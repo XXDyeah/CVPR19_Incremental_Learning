ee17c0cf270a33ab3e7fb635864f3fff6d70d3fd
XXDyeah <55649073+XXDyeah@users.noreply.github.com>
2025-08-04 06:58:25 +0800

    Update compute_features.py

--- utils_incremental/compute_features.py
+++ utils_incremental/compute_features.py
@@ -18,7 +18,7 @@ import argparse
 from PIL import Image
 from scipy.spatial.distance import cdist
 from sklearn.metrics import confusion_matrix
-from utils_pytorch import *
+# from utils_pytorch import *
 
 def compute_features(tg_feature_model, evalloader, num_samples, num_features, device=None):
     if device is None:
@@ -37,7 +37,10 @@ def compute_features(tg_feature_model, evalloader, num_samples, num_features, de
     with torch.no_grad():
         for inputs, targets in evalloader:
             inputs = inputs.to(device)
-            features[start_idx:start_idx+inputs.shape[0], :] = np.squeeze(tg_feature_model(inputs))
+            # features[start_idx:start_idx+inputs.shape[0], :] = np.squeeze(tg_feature_model(inputs))
+            features[start_idx:start_idx + inputs.shape[0], :] = np.squeeze(
+                tg_feature_model(inputs).detach().cpu().numpy()
+            )
             start_idx = start_idx+inputs.shape[0]
     assert(start_idx==num_samples)
     return features
